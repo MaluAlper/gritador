b321c232aaf6d21a83b35aae1ab1441ada3b4c58
Maria Luiza <malualper@hotmail.com>
2020-05-09 20:03:16 -0300

    gritador

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.Write("Digite qualquer palavra ");
+            string textoDigitado = Console.ReadLine();
+
+            Console.WriteLine(textoDigitado.ToUpper());
+            
+            Console.WriteLine("Pressione uma tecla para continuar...");
+            Console.ReadKey();
+        }
+    }
+}
